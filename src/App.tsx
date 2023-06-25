cb9a93c3e674563228f186cf71613ca5252e29e4
OscTec <osctec@hotmail.co.uk>
2023-06-25 12:27:44 +0100

    Add React Vite and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="card">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
